6d5e85456fc5470547a5f0e90b3568328c37b61d
FarruxRavshanov <farruxlynx@gmail.com>
2022-10-17 20:00:33 +0300

    task07

--- string_len07.py
+++ string_len07.py
@@ -8,4 +8,17 @@ def main(s1,s2,s3):
     Returns:
         string
     """
-    return
+    a = len(s1)
+    a1 = str(s1)
+    b = len(s2)
+    b1 = str(s2)
+    c = len(s3)
+    c1 = str(s3)
+    if a % 2 == 1 or b % 2 == 1 or c % 2 == 1:
+        z = '[a1, b1, c1]'
+    if a % 2 == 0 and b % 2 == 0 and c % 2 == 0:
+        z = '[]'
+    
+    return z
+
+print(main('code', 'coder', 'python'))
